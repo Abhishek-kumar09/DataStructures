34202fb63e551f9d6dcd33842ad5f3155af7661e
Abhishekkumar1909 <abhimait1909@gmail.com>
2019-10-24 13:15:56 +0530

    added Binary Tree

--- src/com/DataStructures/Main.java
+++ src/com/DataStructures/Main.java
@@ -1,71 +1,70 @@
 package com.DataStructures;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char ch='Y';
-        Scanner sc= new Scanner(System.in);
+        char ch = 'Y';
+        Scanner sc = new Scanner(System.in);
+        LinkedLists H = new LinkedLists();
+        BinaryTree B= new BinaryTree();
         int choice;
-        int innerchoice , x;
+        int innerchoice, x;
         System.out.println("What do you Want to choose");
         System.out.println("1.Linked List");
-//        Todo: System.out.println("2.Stack");
-//        Todo: System.out.println("3.Queue");
-        choice=sc.nextInt();
-        switch(choice)
-        {
+        System.out.println("2.Stack");
+        System.out.println("3.Queue");
+        System.out.println("4.Binary Tree");
+        choice = sc.nextInt();
+        switch (choice) {
             case 1:
-                   do { LinkedLists list = new LinkedLists();
-                    System.out.println("1.Insert At Beginning");
+                do {
+                    System.out.println("1.Insert At Begening");
                     System.out.println("2.Insert At Ending");
-                    System.out.println("3.Delete At Beginning");
+                    System.out.println("3.Delete At Begenning");
                     System.out.println("4.Delete At Ending");
                     System.out.println("5.Show List");
-                    System.out.println("6. Exit");
-                    innerchoice=sc.nextInt();
-                    switch(innerchoice) {
+                    System.out.println("6.Show Number of Data");
+                    System.out.println("7.Exit");
+                    innerchoice = sc.nextInt();
+                    switch (innerchoice) {
                         case 1:
                             System.out.println("Value You Want to insert at Begenning");
                             x = sc.nextInt();
-                            list.pushBegin(x);
+                            H.pushBegin(x);
                             break;
                         case 2:
                             System.out.println("Value You Want to insert at Ending");
                             x = sc.nextInt();
-                            list.pushEnd(x);
+                            H.pushEnd(x);
                             break;
                         case 3:
-                            int l = list.popBegin().data;
-                            System.out.println("The Data Deleted is : " + l);
+                            H.popBegin();
                             break;
                         case 4:
-                            l = list.popEnd().data;
-                            System.out.println("The Data Deleted is : " + l);
+                            H.popEnd();
                             break;
                         case 5:
-                            System.out.println("The Linked list is : ");
-                            list.showList();
+                            H.showList();
                             break;
-
+                        case 6:
+                            H.pushBegin(1);
+                            break;
                     }
-
-
-//                    System.out.println("Do You Want to Continue");
-//                    ch=sc.next().charAt(0);
-                }while(innerchoice<6);
+                } while (innerchoice < 6);
                 break;
-//            case 2:
-//                do {
-//                    System.out.println("1.Insertion");
-//                    System.out.println("2.Deletion");
-//                    System.out.println("3.Show Stack");
-//                    System.out.println("4.Show Number of Data in Stack");
-//                    innerchoice=sc.nextInt();
-//                    switch(innerchoice) {
-//                        case 1: System.out.println("Value You Want to insert");
+            case 2:
+                do {
+                    System.out.println("1.Insertion");
+                    System.out.println("2.Deletion");
+                    System.out.println("3.Show Stack");
+                    System.out.println("4.Show Number of Data in Stack");
+                    innerchoice = sc.nextInt();
+                    switch (innerchoice) {
+                        case 1:
+                            System.out.println("Value You Want to insert");
 //                            x=sc.nextInt();
-//
+
 //                            H.insertAtEnd(x);
 //                            break;
 //                        case 2: H.deleteAtEnding();
@@ -74,44 +73,191 @@ public class Main {
 //                            break;
 //                        case 4: H.NumberOfNodes();
 //                            break;
-//                    }
-//                    System.out.println("Do You Want to Continue");
-//                    ch=sc.next().charAt(0);
-//                }while(ch=='Y'||ch=='y');
-//                break;
-//            case 3:
-//                do {
-//
-//                    System.out.println("1.Insertion");
-//                    System.out.println("2.Deletion");
-//                    System.out.println("3.Show Queue");
-//                    System.out.println("4.Show Number of Data in Queue");
-//                    innerchoice=sc.nextInt();
-//                    switch(innerchoice) {
-//                        case 1: System.out.println("Value You Want to insert");
-//                            x=sc.nextInt();
-//                            H.insertAtEnd(x);
-//                            break;
-//                        case 2: H.deleteAtBegenning();
-//                            break;
-//                        case 3: H.Show();
-//                            break;
-//                        case 4: H.NumberOfNodes();
-//                            break;
-//                    }
-//                    System.out.println("Do You Want to Continue");
-//                    ch=sc.next().charAt(0);
-//                }while(ch!='N'||ch!='n');
-//                break;
-        
+                    }
+                    System.out.println("Do You Want to Continue");
+                    ch = sc.next().charAt(0);
+                } while (ch == 'Y' || ch == 'y');
+                break;
+            case 3:
+                do {
+                    Queue q = new Queue();
+                    System.out.println("1.Insertion");
+                    System.out.println("2.Deletion");
+                    System.out.println("3.Show Queue");
+                    System.out.println("4.Show Number of Data in Queue");
+                    innerchoice = sc.nextInt();
+                    switch (innerchoice) {
+                        case 1:
+                            System.out.println("Value You Want to insert");
+                            x = sc.nextInt();
+                            q.enQueue(x);
+                            break;
+                        case 2:
+                            x = sc.nextInt();
+                            H.pushEnd(x);
+                            break;
+                        case 3:
+                            H.popBegin();
+                            break;
+                        case 4:
+                            H.popBegin();
+                            break;
+                    }
+                    System.out.println("Do You Want to Continue");
+                    ch = sc.next().charAt(0);
+                } while (ch != 'N' || ch != 'n');
+                break;
+            case 4:
+                do {
+                    Queue q = new Queue();
+                    System.out.println("1.Insertion In BinaryTree");
+                    System.out.println("2.Deletion In Binary Tree");
+                    System.out.println("3.Show Binary Tree in LevelOrder");
+                    System.out.println("4.Show Binary Tree in postOrder");
+
+                    System.out.println("5.Show Number of Data in Tree");
+                    innerchoice = sc.nextInt();
+                    switch (innerchoice) {
+                        case 1:
+                            System.out.println("Value You Want to insert");
+                            x = sc.nextInt();
+                            B.insert(x);
+                            break;
+                        case 2:
+                            x = sc.nextInt();
+                            H.pushEnd(x);
+                            break;
+                        case 3:
+                            B.ShowTree();
+                            break;
+                        case 4:
+                            B.printPostorder(B.root);
+                            break;
+                    }
+                    System.out.println("Do You Want to Continue");
+                    ch = sc.next().charAt(0);
+                } while (ch != 'N' || ch != 'n');
+                break;
+        }
 
     }
 }
-}
 
 
-
-
-
-
-
+//package com.DataStructures;
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        char ch='Y';
+//        Scanner sc= new Scanner(System.in);
+//        int choice;
+//        int innerchoice , x;
+//        System.out.println("What do you Want to choose");
+//        System.out.println("1.Linked List");
+////        Todo: System.out.println("2.Stack");
+////        Todo: System.out.println("3.Queue");
+//        choice=sc.nextInt();
+//        switch(choice)
+//        {
+//            case 1:
+//                   do { LinkedLists list = new LinkedLists();
+//                    System.out.println("1.Insert At Beginning");
+//                    System.out.println("2.Insert At Ending");
+//                    System.out.println("3.Delete At Beginning");
+//                    System.out.println("4.Delete At Ending");
+//                    System.out.println("5.Show List");
+//                    System.out.println("6. Exit");
+//                    sc.nextLine();
+//                    innerchoice=sc.nextInt();
+//                    switch(innerchoice) {
+//
+//                        case 1:sc.nextLine();
+//                            System.out.println("Value You Want to insert at Begenning");
+//                            x = sc.nextInt();
+//                            list.pushBegin(x);
+//                            break;
+//                        case 2:sc.nextLine();
+//                            System.out.println("Value You Want to insert at Ending");
+//                            x = sc.nextInt();
+//                            list.pushEnd(x);
+//                            break;
+//                        case 3:
+//                            int l = list.popBegin().data;
+//                            System.out.println("The Data Deleted is : " + l);
+//                            break;
+//                        case 4:
+//                            l = list.popEnd().data;
+//                            System.out.println("The Data Deleted is : " + l);
+//                            break;
+//                        case 5:
+//                            System.out.println("The Linked list is : ");
+//                            list.showList();
+//                            break;
+//
+//                    }
+//
+//
+////                    System.out.println("Do You Want to Continue");
+////                    ch=sc.next().charAt(0);
+//                }while(innerchoice<6);
+//                break;
+////            case 2:
+////                do {
+////                    System.out.println("1.Insertion");
+////                    System.out.println("2.Deletion");
+////                    System.out.println("3.Show Stack");
+////                    System.out.println("4.Show Number of Data in Stack");
+////                    innerchoice=sc.nextInt();
+////                    switch(innerchoice) {
+////                        case 1: System.out.println("Value You Want to insert");
+////                            x=sc.nextInt();
+////
+////                            H.insertAtEnd(x);
+////                            break;
+////                        case 2: H.deleteAtEnding();
+////                            break;
+////                        case 3: H.Show();
+////                            break;
+////                        case 4: H.NumberOfNodes();
+////                            break;
+////                    }
+////                    System.out.println("Do You Want to Continue");
+////                    ch=sc.next().charAt(0);
+////                }while(ch=='Y'||ch=='y');
+////                break;
+////            case 3:
+////                do {
+////
+////                    System.out.println("1.Insertion");
+////                    System.out.println("2.Deletion");
+////                    System.out.println("3.Show Queue");
+////                    System.out.println("4.Show Number of Data in Queue");
+////                    innerchoice=sc.nextInt();
+////                    switch(innerchoice) {
+////                        case 1: System.out.println("Value You Want to insert");
+////                            x=sc.nextInt();
+////                            H.insertAtEnd(x);
+////                            break;
+////                        case 2: H.deleteAtBegenning();
+////                            break;
+////                        case 3: H.Show();
+////                            break;
+////                        case 4: H.NumberOfNodes();
+////                            break;
+////                    }
+////                    System.out.println("Do You Want to Continue");
+////                    ch=sc.next().charAt(0);
+////                }while(ch!='N'||ch!='n');
+////                break;
+//
+//
+//    }
+//}
+//}
+//
+//
+//
+//
+//
+//
+//
---
+++ src/com/DataStructures/Queue.java
@@ -0,0 +1,12 @@
+package com.DataStructures;
+
+public class Queue {
+    LinkedLists ls = new LinkedLists();
+    void enQueue(int key) {
+        ls.pushBegin(key);
+    }
+    int deQueue() {
+        int temp = ls.popEnd().data;
+        return temp;
+    }
+}
